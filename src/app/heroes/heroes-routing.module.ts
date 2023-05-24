58986aac80f9251b6f48683c868ad70e47f44348
Charles Bravo <chalme220@gmail.com>
2023-05-24 07:47:52 +0200

    menu sidebar heroes

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
         component: NewPageComponent
       },
       {
-        path: ':list',
+        path: 'list',
         component: ListPageComponent
       },
       {
